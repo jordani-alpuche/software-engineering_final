8f1e7a78f82f571544116d5dd325ddd5cce5439e
jordani-alpuche <jordani_a@yahoo.com>
2025-05-20 14:09:26 -0400

    blacklist fix

--- src/app/(pages)/blacklist/selectvisitor/page.tsx
+++ src/app/(pages)/blacklist/selectvisitor/page.tsx
@@ -4,7 +4,6 @@ import { getAllVisitors } from "@/lib/serverActions/visitors/update/UpdateVisito
 import {  redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth"; // Auth logic is moved to a separate file
-import notfound from "@/app/errors/404/page"; // Import the notfound component
 async function page()  {
   const session = await getServerSession(authOptions); // Get session from next-auth
 
@@ -18,14 +17,11 @@ async function page()  {
   const userid = session?.user.id; // Get the user ID from the session
 
   
-  if (Object.keys(visitorGetData).length === 0) {
-    // Check if visitor data is empty
-    return notfound(); // Redirect to 404 page if visitor data is not found
-  } else {
+
     return (
       <SelectVisitorToBlacklist visitorsData={visitorGetData} userid={userid} /> // Pass the fetched visitor data to the component
     );
-  }
+ 
 };
 
 export default page;
